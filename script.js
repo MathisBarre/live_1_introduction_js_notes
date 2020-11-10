acb4578a98330fbd816519cd77a9c3cc143394fd
Mathis Barré <mathis.barre@live.fr>
2020-11-10 20:36:11 +0100

    ajout commentaires

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-var inputElt = document.querySelector(".form__input")
-var formElt = document.querySelector(".form")
-var ulElt = document.querySelector(".notes")
+// Récupérer les éléments HTML nécessaires et les stocker dans des variables
+var inputElt = document.querySelector(".form__input");
+var formElt = document.querySelector(".form");
+var ulElt = document.querySelector(".notes");
 
 function addNote(event) {
-  event.preventDefault()
+  // Permer d'enlever le comportement par défaut de l'événement submit
+  // (aller à la page de l'attribut action de la balise form ou rechargement 
+  // de la page si l'attribut est manquant)
+  event.preventDefault();
 
-  var note = inputElt.value
-  
-  var liElt = document.createElement("li")
-  liElt.textContent = note
+  // Prend la valeur de l'input et la met dans la variable note
+  var note = inputElt.value;
 
-  ulElt.appendChild(liElt)
+  // Créer un élément <li>
+  var liElt = document.createElement("li");
+  // Ajouter le texte de la note en tant que texte de la balise <li>
+  liElt.textContent = note;
 
-  inputElt.value = ""
+  // Ajouter l'élément <li> nouvellement créé dans l'élément <ul class="notes">
+  ulElt.appendChild(liElt);
+
+  // Supprimer le texte de l'élément input
+  inputElt.value = "";
 }
 
-formElt.addEventListener("submit", addNote)
+// Accrocher la fonction addNote à l'évenement submit de l'élément <form>
+formElt.addEventListener("submit", addNote);
